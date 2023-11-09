677b12d8b747bd16fe0d0779baa289d86960abbd
sonan2 <sonan@bc.edu>
2023-11-09 00:55:56 -0500

    bar graph

---
+++ bargraph.js
@@ -0,0 +1,38 @@
+function Count() {
+    var svg = d3.select("svg"),
+    margin = 350,
+    width = svg.attr("width") - margin,
+    height = svg.attr("height") - margin;
+
+    var xScale = d3.scaleBand().range([0,width]).padding(0.4);
+    var yScale = d3.scaleLinear().range([height,0]);
+
+    var g = svg.append("g").attr("transform", "translate("+100+","+100+")");
+
+    d3.csv("Count-Only.csv").then(data =>{
+
+        xScale.domain(data.map(d => d.SPECIES));
+        yScale.domain([0, 3000]);
+
+        g.append("g")
+            .attr('transform', 'translate(0, '+height+')' )
+            .call(d3.axisBottom(xScale))
+            .selectAll("text")  
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", ".15em")
+            .attr("transform", "rotate(-70)");
+    
+        g.append("g").call(d3.axisLeft(yScale));
+
+        g.selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", d => xScale(d.SPECIES))
+            .attr("y", d => yScale(d.COUNT))
+            .attr("width", xScale.bandwidth())
+            .attr("height", d => height - yScale(d.COUNT))
+    })  
+}
+
